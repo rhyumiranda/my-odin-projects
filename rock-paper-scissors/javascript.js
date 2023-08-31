8980ee1a4a7272bd3d1701f2d9d6d0406e0fa214
Rhyu Miranda <mirandarhyu@gmail.com>
2023-08-31 11:48:05 +0800

    Update file name and add basic functions for game

---
+++ rock-paper-scissors/javascript.js
@@ -0,0 +1,13 @@
+function playround(playerSelection, computerSelection){
+
+}
+
+
+function getComputerChoice(){
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playround(playerSelection, computerSelection));
+
