d00869b0ed27e75230048470816210aaa5f88860
Rhyu Miranda <mirandarhyu@gmail.com>
2023-08-31 13:50:29 +0800

    Add functions for game looping and winner decision

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,25 +1,73 @@
+function game(){
 
+    let playerScore = 0;
+    let computerScore = 0;
+    let tieScore = 0;
 
-//function playround(playerSelection, computerSelection){
+    for (let i = 0; i < 5; i++){
+        const playerSelection = getUserChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        if (getWinner(playerSelection, computerSelection) == "Player"){
+            playerScore++;
+        } else if(getWinner(playerSelection, computerSelection) == "Computer"){
+            computerScore++;
+        } else if(getWinner(playerSelection, computerSelection) == "Tie"){
+            tieScore++;
+        }
+    }
+}
 
-//}
+function playRound(playerSelection, computerSelection){
+    const winner = getWinner(playerSelection, computerSelection);
 
-//function playerSelection(){
+    if (winner == "Tie"){
+        console.log("It is a tie!");
+    } else if (winner == "Player"){
+        console.log(`You win ${playerSelection} beats ${computerSelection}`);
+    } else if (winner == "Computer"){
+        console.log(`You lost, ${computerSelection} beats ${playerSelection}`);
+    }
+}
 
-//}
+function getUserChoice(){
+    const userInput = false;
+    
+    while(userInput == false){
+        const userChoice = prompt("Rock Paper Scissors");
+        if(userChoice == null){
+            continue;
+        }
+        const userChoiceLower = userChoice.toLowerCase();
+        if (options.includes(userChoiceLower)){
+            userInput == true;
+            return userChoiceLower;
+        }
+    }       
+}
 
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     const choices = options[Math.floor(Math.random() * options.length)];
-    console.log(choices);
+    return choices;
 }
 
-//function getWinner(){
+function getWinner(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        return "Tie";
+    } else if(
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ){
+        return "Player";
+    } else {
+        return "Computer";
+    }
+}
 
-//}
-
-//const playerSelection = "rock";
-//const computerSelection = getComputerChoice();
-//console.log(playround(playerSelection, computerSelection));
-
+const playerSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
+game();
