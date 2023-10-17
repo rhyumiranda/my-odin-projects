5acab1f9676272c8031a0fd67203c7b79e43cd8e
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-10-17 21:11:31 +0800

    Rework of code and new UI using DOM and updated script

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -3,11 +3,11 @@ function game(){
     let computerScore = 0;
 
     function playRound() {
-        const rockBtn = document.querySelector('.rock');
-        const paperBtn = document.querySelecctor('.paper');
-        const scisssorBtn = document.querySelector('.scissor');
+        const rockBtn = document.querySelector('#rock');
+        const paperBtn = document.querySelector('#paper');
+        const scisssorBtn = document.querySelector('#scissors');
         const playerOptions = [rockBtn,paperBtn,scisssorBtn];
-        const computerOptions = ['rock', 'paper', 'scissors']
+        const computerOptions = ['rock', 'paper', 'scissors'];
 
         playerOptions.forEach(option => {
             option.addEventListener('click', function(){
@@ -15,10 +15,39 @@ function game(){
                 const choiceNumber = Math.floor(Math.random()*3);
                 const computerChoice = computerOptions[choiceNumber];
 
-                /* winner */
-            })
-        })
+                winner(this.innerText, computerChoice);
+
+            });
+        });
 
     }
 
+    function winner(player, computer) {
+        const result = document.querySelector('.result');
+        const playerScoreBoard = document.querySelector('#playerText');
+        const computerScoreBoard = document.querySelector('#computerText');
+        player = player.toLowerCase();
+        computer = computer.toLowerCase();
+
+        if(player === computer){
+            result.textContent = 'Tie';
+        } else if (
+        player === 'rock' && computer === 'scissor' ||
+        player === 'paper' && computer === 'rock' ||
+        player === 'scissors' && computer === 'paper'
+        ){
+            result.textContent = 'Player Won';
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        } else {
+            result.textContent = 'Computer Won';
+            computerScore++;
+            computerScoreBoard.textContent = computerScore;
+        }
+    }
+
+    playRound();
+
 }
+
+game();
