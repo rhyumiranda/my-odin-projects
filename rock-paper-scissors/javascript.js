a0cc766566439c3614edc1e932eea9cdeff7d8f3
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-10-18 21:27:24 +0800

    Fix git conflict

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-let startButton = document.getElementById("startGame");
-
-startButton.addEventListener("click", function(){
-    game();
-});
-
-=======
->>>>>>> rps-ui
 function game(){
     let playerScore = 0;
     let computerScore = 0;
@@ -55,69 +46,8 @@ function game(){
         }
     }
 
-<<<<<<< HEAD
-    console.log("Game over!");
-    if(playerScore > computerScore){
-        console.log(`You are the winner! With a score of ${playerScore} points`);
-    } else if (playerScore < computerScore){
-        console.log(`You lost, computer won! With a score of ${computerScore} points`);
-    } else if (playerScore == computerScore){
-        console.log("It is a tie, nobody won!");
-    }
-}
-
-function playRound(playerSelection, computerSelection){
-    const winner = getWinner(playerSelection, computerSelection);
-
-    if (winner == "Tie"){
-        console.log("It is a tie!");
-    } else if (winner == "Player"){
-        console.log(`You win ${playerSelection} beats ${computerSelection}`);
-    } else if (winner == "Computer"){
-        console.log(`You lost, ${computerSelection} beats ${playerSelection}`);
-    }
-}
-
-function getUserChoice(){
-    const userInput = false;
-    
-    while(userInput == false){
-        const userChoice = prompt("Rock Paper Scissors");
-        if(userChoice == null){
-            continue;
-        }
-        const userChoiceLower = userChoice.toLowerCase();
-        if (options.includes(userChoiceLower)){
-            userInput == true;
-            return userChoiceLower;
-        }
-    }       
-}
-
-const options = ["rock", "paper", "scissors"];
-
-function getComputerChoice(){
-    const choices = options[Math.floor(Math.random() * options.length)];
-    return choices;
-}
-
-function getWinner(playerSelection, computerSelection){
-    if(playerSelection === computerSelection){
-        return "Tie";
-    } else if(
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ){
-        return "Player";
-    } else {
-        return "Computer";
-    }
-} 
-=======
     playRound();
 
 }
 
 game();
->>>>>>> rps-ui
