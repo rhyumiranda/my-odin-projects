c87611b2b57218df4a63bcfd1e2825972a5575a7
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-12-15 12:41:35 +0800

    Create an outline of the etch-a-sketch

---
+++ etch-a-sketch/js/script.js
@@ -0,0 +1,7 @@
+//Create a canvas that is made out of grid
+//Have a slider where users are able to slide and choose number of grids
+//Users are able to draw on hovering the canvas
+//Users can erase their drawing using eraser tool
+//Users can reset the canvas
+
+console.log("Hello World")
