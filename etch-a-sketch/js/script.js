91e31a9643cac302df23de354187f32017c05387
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-12-15 19:27:25 +0800

    Add a function to create a grid inside the container

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -4,4 +4,28 @@
 //Users can erase their drawing using eraser tool
 //Users can reset the canvas
 
-console.log("Hello World")
+function createGrid(x){
+  for(let row = 0; row < x; row++){
+    for(let column = 0; column < x; column++){
+      let gridContainer = document.querySelector("#gridContainer");
+      let grid = document.createElement("div");
+      grid.className = "grid";
+      gridContainer.appendChild(grid);
+
+    };
+  };
+  let gridElements = document.querySelectorAll(".grid");
+  gridElements.forEach(function(element) {
+    element.style.height = (650 / x) + 'px';
+  });
+  gridElements.forEach(function(element) {
+    element.style.width = (650 / x) + 'px';
+  });
+};
+
+function refreshGrid(){
+  let newGrid = prompt("How many boxes of grid would you like to add?");
+  createGrid(newGrid);
+}
+
+createGrid(20);
