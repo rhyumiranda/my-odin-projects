c88fdbd2c76c47b1bd40d54ec47de9557264e89e
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-01-06 00:58:44 +0800

    Add basic functions for operations

---
+++ calculator/js/script.js
@@ -0,0 +1,16 @@
+
+const add = (a, b) =>{
+  return alert(a + b);
+}
+
+const subtract = (a, b) =>{
+  return alert(a - b);
+}
+
+const multiply = (a, b) =>{
+  return alert(a * b);
+}
+
+const divide = (a, b) =>{
+  return alert(a / b);
+}
