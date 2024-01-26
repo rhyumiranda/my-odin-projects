73520a3eb38b793298b2fcba4047d8bc4afe5185
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-01-27 01:23:43 +0800

    Create a function to open menu

---
+++ odin-recipes/js/script.js
@@ -0,0 +1,11 @@
+function showSidebar(){
+  let sidebar = document.querySelector('.sidebar');
+
+  sidebar.style.display = 'flex';
+}
+
+function hideSidebar(){
+  let sidebar = document.querySelector('.sidebar');
+
+  sidebar.style.display = 'none';
+}
